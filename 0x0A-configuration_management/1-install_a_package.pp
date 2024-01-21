ae626caf77f2400047783aeb94a6e2d9aa68418c
solomonadola <solomonadola84@gmail.com>
2024-01-21 11:30:13 -0800

    Install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# puppet declarative script to grab puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
