9cf0ffe93652296db1ed350eb71f54b78e209add
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2024-06-22 22:06:10 +0300

    Task 0 rework

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
-"""contains number_of_subscribers"""
+
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number
-    of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (requests.RequestException, KeyError):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+                         v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
