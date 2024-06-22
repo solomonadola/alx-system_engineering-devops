f0cd476e4dd0d4a2d56590c633bd74d61b5e92ff
solomonadola <solomonadola84@gmail.com>
2024-06-22 08:20:50 -0700

    Test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 import requests
-"""contains number_of_subscribers function"""
-
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit. If the subreddit is invalid, returns 0.
+    Queries the Reddit API and returns the number
+    of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom User Agent"}
-
     try:
         response = requests.get(url, headers=headers)
         data = response.json()
         return data["data"]["subscribers"]
     except (requests.RequestException, KeyError):
         return 0
+
