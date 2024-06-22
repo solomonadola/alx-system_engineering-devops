4c39e9654a2b352d9b8508df29f80b99db4881f5
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2024-06-22 22:15:29 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,16 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit."""
     if subreddit is None or type(subreddit) is not str:
         return 0
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-                         v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+                     headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
+
+# Example usage:
+if __name__ == "__main__":
+    subreddit_name = "programming"  # Replace with your desired subreddit
+    print("Number of subscribers:", number_of_subscribers(subreddit_name))
