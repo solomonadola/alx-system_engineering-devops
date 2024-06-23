4424e3151603e645ac81768ac6e00fbfc0ae42d6
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2024-06-23 08:08:05 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API and get the number of subscribers for a subreddit.
+Module to query the Reddit API and
+get the number of subscribers for a subreddit.
 """
 
 import requests
@@ -8,18 +9,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given,
+    the function returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'python:subscribers.counter:v1.0 (by /u/Silver_Oil1466)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+    headers = {"User-Agent": "v1.0 (by /u/Silver_Oil1466)"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data").get("subscribers")
+    else:
         return 0
-
